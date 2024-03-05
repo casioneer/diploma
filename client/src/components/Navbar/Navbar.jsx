d45ff56901b401c3b392e7c8790f0c1aaca2e6e0
casioneer <xomutman@gmail.com>
2024-03-05 12:42:57 +0300

    hello

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import style from './Navbar.module.css'
+import Logo from '../../assets/logo.png'
+import LogoText from '../../assets/logo_text.png'
+import { AiOutlineMenu } from 'react-icons/ai'
+
+export default function Navbar() {
+    return (
+        <header className={style.header}>
+            <div className='container'>
+                <div className={style.box}>
+                    <div className={style.logo_image}>
+                        <img src={Logo} alt="/" />
+                        <img src={LogoText} alt="/" />
+                    </div>
+                    <ul className={style.menu}>
+                        <li>
+                            <a href="##">Главная</a>
+                        </li>
+                        <li>
+                            <a href="##">Меню</a>
+                        </li>
+                        <li>
+                            <a href="##">О нас</a>
+                        </li>
+                        <li>
+                            <a href="##">Контакты</a>
+                        </li>
+                        <li>
+                            <a href="##">Войти</a>
+                        </li>
+                    </ul>
+                    <div>
+                        <AiOutlineMenu size={25} />
+                    </div>
+                </div>
+            </div>
+        </header >
+    )
+}
---
+++ makefile
@@ -0,0 +1,16 @@
+SHELL = /bin/sh
+.PHONY: b
+b:
+	docker compose -f ./docker-compose.dev.yml up --build
+
+.PHONY: cu
+cu:
+	docker compose -f ./docker-compose.dev.yml up
+
+.PHONY: release-build
+release-build:
+	docker compose -f ./docker-compose.yml up --build
+
+.PHONY: release-up
+release-up:
+	docker compose -f ./docker-compose.yml up
